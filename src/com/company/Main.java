86cd577528f9e71edd0f1e9b27f0e5e75502575c
hoale0507 <hoale090293@gmail.com>
2022-01-10 22:06:31 +0700

    add file

---
+++ src/com/company/Main.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Student student = new Student();
+        student.setName("Hoa");
+        student.setClasses("C1121G1");
+    }
+}
